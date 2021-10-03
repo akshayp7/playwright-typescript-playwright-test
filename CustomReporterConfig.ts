840a06089c224f2209cc4f5437a53e91177863e5
akshayp7 <codendeavor@gmail.com>
2021-10-03 15:46:20 +0530

    CustomReportErrorLog

--- CustomReporterConfig.ts
+++ CustomReporterConfig.ts
@@ -1,22 +1,22 @@
 import { Reporter, TestCase, TestError, TestResult, TestStep } from "@playwright/test/reporter";
 
-export default class CustomReporterConfig {
+export default class CustomReporterConfig implements Reporter {
 
     onTestBegin(test: TestCase): void {
         console.log(`Test Case Started : ${test.title}`);
     }
 
-    onTestEnd?(test: TestCase, result: TestResult): void {
+    onTestEnd(test: TestCase, result: TestResult): void {
         console.log(`Test Case Completed : ${test.title} Status : ${result.status}`);
     }
 
-    onStepBegin?(test: TestCase, result: TestResult, step: TestStep): void {
+    onStepBegin(test: TestCase, result: TestResult, step: TestStep): void {
         if (step.category === `test.step`) {
             console.log(`Executing Step : ${step.title}`);
         }
     }
 
-    onError(error: TestError) {
+    onError(error: TestError): void {
         console.log(error.message);
     }
 }
