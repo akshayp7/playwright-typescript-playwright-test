5d17bc6ed63979e207f3d8b8da19e0b4a4145baf
akshayp7 <codendeavor@gmail.com>
2024-02-24 15:09:28 +0530

    GithubActions_DockerfileChanges

--- global-teardown.ts
+++ global-teardown.ts
@@ -2,10 +2,10 @@ import path from 'path';
 import AdmZip from 'adm-zip';
 
 async function globalTeardown() {
-    // const reportPath = path.join(__dirname, `html-report`);
-    // const zip = new AdmZip();
-    // zip.addLocalFolder(reportPath, `./html-report`);
-    // zip.writeZip(`./html-report.zip`);
+    const reportPath = path.join(__dirname, `html-report`);
+    const zip = new AdmZip();
+    zip.addLocalFolder(reportPath, `./html-report`);
+    zip.writeZip(`./html-report.zip`);
 }
 
 export default globalTeardown;
