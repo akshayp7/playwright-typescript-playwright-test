8cd269aa3ba3ece852b359a0e6f6fe89f3146fc3
akshayp7 <codendeavor@gmail.com>
2024-02-24 17:18:48 +0530

    ChangedGithubActions

--- global-setup.ts
+++ global-setup.ts
@@ -3,7 +3,6 @@ import rimraf from "rimraf";
 async function globalSetup(): Promise<void> {
     await new Promise(resolve => {
         rimraf(`./allure-results`, resolve);
-        rimraf(`./html-report`, resolve);
     });
 }
 export default globalSetup;
